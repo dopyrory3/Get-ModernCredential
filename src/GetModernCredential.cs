f85a12e3fc2975e3ecb4bb798de9f02809658ff5
Rory Maher <rmaher229@gmail.com>
2020-05-08 15:57:23 +0100

    Create GetModernCredential.cs
    Upload DLL source

---
+++ src/GetModernCredential.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Management.Automation;
+
+namespace psAuth
+{
+    [Cmdlet(VerbsCommon.Get, "ModernCredential")]
+    [OutputType(typeof(NetworkCredential))]
+    public class PSCredUI: Cmdlet
+    {
+        private PSCredential credential;
+
+        [DllImport("credui.dll", CharSet = CharSet.Unicode)]
+        private static extern uint CredUIPromptForWindowsCredentials(
+            ref CREDUI_INFO notUsedHere,
+            int authError,
+            ref uint authPackage,
+            IntPtr InAuthBuffer,
+            uint InAuthBufferSize,
+            out IntPtr refOutAuthBuffer,
+            out uint refOutAuthBufferSize,
+            ref bool fSave,
+            PromptForWindowsCredentialsFlags flags);
+
+        [DllImport("credui.dll", CharSet = CharSet.Unicode)]
+        private static extern bool CredUnPackAuthenticationBuffer(
+            int dwFlags,
+            IntPtr pAuthBuffer,
+            uint cbAuthBuffer,
+            StringBuilder pszUserName,
+            ref int pcchMaxUserName,
+            StringBuilder pszDomainName,
+            ref int pcchMaxDomainame,
+            StringBuilder pszPassword,
+            ref int pcchMaxPassword);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct CREDUI_INFO
+        {
+            public int cbSize;
+            public IntPtr hwndParent;
+            public string pszMessageText;
+            public string pszCaptionText;
+            public IntPtr hbmBanner;
+        }
+        [Flags]
+        private enum PromptForWindowsCredentialsFlags
+        {
+            /// <summary>
+            /// The caller is requesting that the credential provider return the user name and password in plain text.
+            /// This value cannot be combined with SECURE_PROMPT.
+            /// </summary>
+            CREDUIWIN_GENERIC = 0x1,
+            /// <summary>
+            /// The Save check box is displayed in the dialog box.
+            /// </summary>
+            CREDUIWIN_CHECKBOX = 0x2,
+            /// <summary>
+            /// Only credential providers that support the authentication package specified by the authPackage parameter should be enumerated.
+            /// This value cannot be combined with CREDUIWIN_IN_CRED_ONLY.
+            /// </summary>
+            CREDUIWIN_AUTHPACKAGE_ONLY = 0x10,
+            /// <summary>
+            /// Only the credentials specified by the InAuthBuffer parameter for the authentication package specified by the authPackage parameter should be enumerated.
+            /// If this flag is set, and the InAuthBuffer parameter is NULL, the function fails.
+            /// This value cannot be combined with CREDUIWIN_AUTHPACKAGE_ONLY.
+            /// </summary>
+            CREDUIWIN_IN_CRED_ONLY = 0x20,
+            /// <summary>
+            /// Credential providers should enumerate only administrators. This value is intended for User Account Control (UAC) purposes only. We recommend that external callers not set this flag.
+            /// </summary>
+            CREDUIWIN_ENUMERATE_ADMINS = 0x100,
+            /// <summary>
+            /// Only the incoming credentials for the authentication package specified by the authPackage parameter should be enumerated.
+            /// </summary>
+            CREDUIWIN_ENUMERATE_CURRENT_USER = 0x200,
+            /// <summary>
+            /// The credential dialog box should be displayed on the secure desktop. This value cannot be combined with CREDUIWIN_GENERIC.
+            /// Windows Vista: This value is not supported until Windows Vista with SP1.
+            /// </summary>
+            CREDUIWIN_SECURE_PROMPT = 0x1000,
+            /// <summary>
+            /// The credential provider should align the credential BLOB pointed to by the refOutAuthBuffer parameter to a 32-bit boundary, even if the provider is running on a 64-bit system.
+            /// </summary>
+            CREDUIWIN_PACK_32_WOW = 0x10000000,
+        }
+
+        protected override void EndProcessing()
+        {
+            // Create a new CREDUI_INFO object & set default properties
+            CREDUI_INFO credui = new CREDUI_INFO();
+            credui.pszCaptionText = "Please enter your credentials";
+            credui.pszMessageText = "";
+            credui.cbSize = Marshal.SizeOf(credui);
+
+            uint authPackage = 0;
+            IntPtr outCredBuffer = new IntPtr();
+            uint outCredSize;
+
+            // Instantaniate the credential flags
+            PromptForWindowsCredentialsFlags flags = new PromptForWindowsCredentialsFlags();
+
+            // Launch the credential window
+            bool save = false;
+            uint result = CredUIPromptForWindowsCredentials(
+                ref credui,
+                0,
+                ref authPackage,
+                IntPtr.Zero,
+                0,
+                out outCredBuffer,
+                out outCredSize,
+                ref save,
+                flags);
+
+            // Create the buffers
+            var usernameBuf = new StringBuilder(100);
+            var passwordBuf = new StringBuilder(100);
+            var domainBuf = new StringBuilder(100);
+
+            int maxUserName = 100;
+            int maxDomain = 100;
+            int maxPassword = 100;
+
+            if (result == 0)
+            {
+                if (CredUnPackAuthenticationBuffer(0, outCredBuffer, outCredSize, usernameBuf, ref maxUserName, domainBuf, ref maxDomain, passwordBuf, ref maxPassword))
+                {
+                    // Create a new PSCredential
+                    SecureString secure = new SecureString();
+                    char[] chars = passwordBuf.ToString().ToCharArray();
+                    foreach (char ch in chars)
+                    {
+                        secure.AppendChar(ch);
+                    }
+                    credential = new PSCredential(usernameBuf.ToString(), secure);
+                }
+            }
+            else
+            {
+                // Throw an error
+                ErrorRecord error = new ErrorRecord(
+                    new Exception("Credentials were not provided or the dialog was closed unexpectedly"),
+                    "UnexpectedResult",
+                    ErrorCategory.CloseError,
+                    result);
+                ThrowTerminatingError(error);
+            }
+
+            WriteObject(credential);
+        }
+    }
+}
